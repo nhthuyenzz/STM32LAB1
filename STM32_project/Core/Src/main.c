14b98c5d9ef079ac51c5e1c7d840f512ddeec044
nhthuyenzz <thuyen.phamdaonhat@hcmut.edu.vn>
2024-09-08 00:31:48 +0700

    update for ex1

--- STM32_project/Core/Src/main.c
+++ STM32_project/Core/Src/main.c
@@ -64,6 +64,7 @@ int main(void)
 {
   /* USER CODE BEGIN 1 */
 	// TODO EX1
+	// OK TEST !!!
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
